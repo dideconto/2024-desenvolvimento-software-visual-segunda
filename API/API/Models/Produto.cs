bca8371754203543e9df6e1c4876f4f17fb0a346
Diogo Steinke Deconto <dideconto@gmail.com>
2024-09-30 22:01:53 -0300

    API - CRUD de produtos com banco de dados

--- API/API/Models/Produto.cs
+++ API/API/Models/Produto.cs
@@ -9,6 +9,7 @@ public class Produto
     }
     public string? Id { get; set; }
     public string? Nome { get; set; }
+    public string? Descricao { get; set; }
     public double Valor { get; set; }
     public int Quantidade { get; set; }
     public DateTime CriadoEm { get; set; }
--- API/API/Program.cs
+++ API/API/Program.cs
@@ -7,6 +7,10 @@ using API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Adicionando o serviço de banco de dados na aplicação
+builder.Services.AddDbContext<AppDataContext>();
+
 var app = builder.Build();
 
 List<Produto> produtos = new List<Produto>
@@ -30,19 +34,27 @@ List<Produto> produtos = new List<Produto>
 app.MapGet("/", () => "API de Produtos");
 
 //GET: /api/produto/listar
-app.MapGet("/api/produto/listar", () =>
+app.MapGet("/api/produto/listar", ([FromServices] AppDataContext ctx) =>
 {
-    if (produtos.Count > 0)
+    if (ctx.Produtos.Any())
     {
-        return Results.Ok(produtos);
+        return Results.Ok(ctx.Produtos.ToList());
     }
     return Results.NotFound();
 });
 
 //GET: /api/produto/buscar/{id}
-app.MapGet("/api/produto/buscar/{id}", ([FromRoute] string id) =>
+app.MapGet("/api/produto/buscar/{id}", ([FromRoute] string id,
+    [FromServices] AppDataContext ctx) =>
 {
-    Produto? produto = produtos.Find(x => x.Id == id);
+    //Buscar pela PK
+    Produto? produto = ctx.Produtos.Find(id);
+    //Buscar por qualquer campo que não seja a PK
+    //Produto? produto = produtos.FirstOrDefault(x => x.Id == id);
+
+    //Filtrar por qualquer campo da sua tabela   
+    //List<Produto> lista = ctx.Produtos.Where(x => x.Quantidade > 10).ToList();
+
     if (produto == null)
     {
         return Results.NotFound();
@@ -50,29 +62,34 @@ app.MapGet("/api/produto/buscar/{id}", ([FromRoute] string id) =>
     return Results.Ok(produto);
 });
 
-//POST: /api/produto/cadastrar/param_nome
-app.MapPost("/api/produto/cadastrar", ([FromBody] Produto produto) =>
+//POST: /api/produto/cadastrar
+app.MapPost("/api/produto/cadastrar", ([FromBody] Produto produto,
+    [FromServices] AppDataContext ctx) =>
 {
-    produtos.Add(produto);
+    ctx.Produtos.Add(produto);
+    ctx.SaveChanges();
     return Results.Created("", produto);
 });
 
 //DELETE: /api/produto/deletar/{id}
-app.MapDelete("/api/produto/deletar/{id}", ([FromRoute] string id) =>
+app.MapDelete("/api/produto/deletar/{id}", ([FromRoute] string id,
+    [FromServices] AppDataContext ctx) =>
 {
-    Produto? produto = produtos.Find(x => x.Id == id);
+    Produto? produto = ctx.Produtos.Find(id);
     if (produto == null)
     {
         return Results.NotFound();
     }
-    produtos.Remove(produto);
+    ctx.Produtos.Remove(produto);
+    ctx.SaveChanges();
     return Results.Ok(produto);
 });
 
 //PUT: /api/produto/alterar/{id}
-app.MapPut("/api/produto/alterar/{id}", ([FromRoute] string id, [FromBody] Produto produtoAlterado) =>
+app.MapPut("/api/produto/alterar/{id}", ([FromRoute] string id,
+    [FromBody] Produto produtoAlterado, [FromServices] AppDataContext ctx) =>
 {
-    Produto? produto = produtos.Find(x => x.Id == id);
+    Produto? produto = ctx.Produtos.Find(id);
     if (produto == null)
     {
         return Results.NotFound();
@@ -80,13 +97,10 @@ app.MapPut("/api/produto/alterar/{id}", ([FromRoute] string id, [FromBody] Produ
     produto.Nome = produtoAlterado.Nome;
     produto.Quantidade = produtoAlterado.Quantidade;
     produto.Valor = produtoAlterado.Valor;
+    ctx.Produtos.Update(produto)ç
+    ctx.SaveChanges();
     return Results.Ok(produto);
 });
 
 
 app.Run();
-
-//Exercícios para aula de hoje
-// - Buscar um produto pelo nome
-// - Remover um produto
-// - Alterar um produto
