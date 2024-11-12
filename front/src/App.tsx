67a52515c4853779897a938a4838b758deae3c77
dideconto <dideconto@gmail.com>
2024-11-11 21:33:40 -0300

    FRONT - Cadastro e listagem de produtos

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import ConsultarCEP from "./ConsultarCEP";
-import ComponenteExemplo from "./ComponenteExemplo";
-import ProdutoListar from "./ProdutoListar";
+import ConsultarCEP from "./components/pages/exemplos/ConsultarCEP";
+import ComponenteExemplo from "./components/pages/exemplos/ComponenteExemplo";
+import ProdutoListar from "./components/pages/produto/ProdutoListar";
+import ProdutoCadastrar from "./components/pages/produto/ProdutoCadastrar";
 
 function App() {
   return (
     <div id="app">
       {/* <ConsultarCEP /> */}
+      <ProdutoCadastrar />
       <ProdutoListar />
     </div>
   );
